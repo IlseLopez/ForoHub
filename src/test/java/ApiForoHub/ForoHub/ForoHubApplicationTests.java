6a7e602f6cfda5d67d080e65a1caf6761aedd855
Ilse Marilu <ilseelias31@gmail.com>
2025-08-13 00:38:33 -0600

    Version inicial proyecto

---
+++ src/test/java/ApiForoHub/ForoHub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiForoHub.ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
